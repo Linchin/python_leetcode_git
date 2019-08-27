6b3e41229f19726a80ca5f71f387708b2554723a
Linchin <lingqing.gan@gmail.com>
2019-08-27 11:50:23 -0400

    Q004 done
    1. divide and conquer algo
    2. list slice is O(n) in python
    3. the really annoying part is the corner cases

--- Q004.py
+++ Q004.py
@@ -1,35 +1,210 @@
-__author__ = 'linchin'
+"""
+Q146 Median of Two Sorted Arrays
+Hard
+Done. :)
 
+array; binary search; divide and conquer.
 
-def findMedianSortedArrays(self, A, B):
-    l = len(A) + len(B)
-    if l % 2 == 1:
-        # odd
-        return self.kth(A, B, l // 2)
-    else:
-        # even
-        return (self.kth(A, B, l // 2) + self.kth(A, B, l // 2 - 1)) / 2.
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 
-def kth(self, a, b, k):
-    if not a:
-        return b[k]
-    if not b:
-        return a[k]
-    ia, ib = len(a) // 2, len(b) // 2
-    ma, mb = a[ia], b[ib]
+You may assume nums1 and nums2 cannot be both empty.
 
-    # when k is bigger than the sum of a and b's median indices
-    if ia + ib < k:
-        # if a's median is bigger than b's, b's first half doesn't include k
-        if ma > mb:
-            return self.kth(a, b[ib + 1:], k - ib - 1)
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+"""
+
+from typing import List
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
+        m = len(nums1)
+        n = len(nums2)
+
+        if m == 0:
+            if n%2 == 1:
+                return nums2[int((n+1)//2-1)]
+            else:
+                return (nums2[int((n+1)//2-1)] + nums2[int((n+1)//2)])/2
+        if n == 0:
+            if m%2 == 1:
+                return nums1[int((m+1)//2-1)]
+            else:
+                return (nums1[int((m + 1) // 2 - 1)] + nums1[int((m + 1) // 2)]) / 2
+
+        if m > n:
+            nums1, nums2 = nums2, nums1
+            m, n = n, m
+
+        l = 0
+        h = m
+
+        if (m + n) % 2 == 0:
+            # even
+
+            while 1:
+
+                p1 = int((l + h) // 2)          # number of elements on the left
+                p2 = int((m + n) / 2 - p1)
+
+                if p1 == 0 and p2 == n:
+                    return (nums1[0] + nums2[-1])/2
+
+                if p1 == m and p2 == 0:
+                    return (nums1[-1] + nums2[0]) / 2
+
+                if p1 == m:
+                    if nums1[-1] <= nums2[p2]:
+                        print("a")
+                        return (max(nums1[-1], nums2[p2-1]) + nums2[p2])/2
+                    else:
+                        h = p1
+                        continue
+
+                if p2 == n:
+                    if nums2[-1] <= nums1[p1]:
+                        return (max(nums2[-1], nums1[p1-1]) + nums1[p1])/2
+                    else:
+                        l = p1
+                        continue
+
+                left1 = nums1[p1-1]
+                left2 = nums2[p2-1]
+                right1 = nums1[p1]
+                right2 = nums2[p2]
+
+                if p1 == 0:
+                    if left2 <= right1:
+                        return (left2 + min(right1, right2))/2
+                    else:
+                        if l + 1 == h:
+                            l = h
+                            continue
+                        else:
+                            l = p1
+                            continue
+
+                if p2 == 0:
+                    if left1 <= right2:
+                        return (left1 + min(right1, right2))/2
+                    else:
+                        h = p1
+                        continue
+
+                if l+1 == h:
+                    if left1 <= right2 and left2 <= right1:
+                        return (max(left1, left2) + min(right1, right2)) / 2
+                    else:
+                        l = h
+                        continue
+
+                if left1 <= right2 and left2 <= right1:
+                    return (max(left1, left2) + min(right1, right2))/2
+
+                elif left1 >= right2:
+                    h = p1
+                    continue
+                elif left1 <= right2:
+                    l = p1
+                    continue
+
         else:
-            return self.kth(a[ia + 1:], b, k - ia - 1)
-    # when k is smaller than the sum of a and b's indices
-    else:
-        # if a's median is bigger than b's, a's second half doesn't include k
-        if ma > mb:
-            return self.kth(a[:ia], b, k)
-        else:
-            return self.kth(a, b[:ib], k)
+            # odd
+
+            while 1:
+
+                p1 = int((l + h) // 2)  # number of elements on the left
+                p2 = int((m + n + 1) / 2 - p1)
+
+                if p1 == 0 and p2 == n:
+                    if nums1[0] >= nums2[-1]:
+                        return nums2[-1]
+                    else:
+                        l = h
+                        continue
+
+                if p1 == m and p2 == 0:
+                    return nums1[-1]
+
+                if p1 == m:
+                    if nums1[-1] <= nums2[p2]:
+                        return max(nums1[-1], nums2[p2-1])
+                    else:
+                        h = p1
+                        continue
+
+                if p2 == n:
+                    if nums2[-1] <= nums1[p1]:
+                        return max(nums1[p1-1], nums2[-1])
+                    else:
+                        l = p1
+                        continue
+
+                left1 = nums1[p1 - 1]
+                left2 = nums2[p2 - 1]
+                right1 = nums1[p1]
+                right2 = nums2[p2]
+
+                if p1 == 0:
+                    if left2 <= right1:
+                        return left2
+                    else:
+                        if l + 1 == h:
+                            l = h
+                            continue
+                        else:
+                            l = p1
+                            continue
+
+                if p2 == 0:
+                    if left1 <= right2:
+                        return (left1 + min(right1, right2)) / 2
+                    else:
+                        h = p1
+                        continue
+
+                if l + 1 == h:
+                    if left1 <= right2 and left2 <= right1:
+                        return max(left1, left2)
+                    else:
+                        l = h
+                        continue
+
+                if left1 <= right2 and left2 <= right1:
+                    return max(left1, left2)
+
+                elif left1 >= right2:
+                    h = p1
+                    continue
+                elif left1 <= right2:
+                    l = p1
+                    continue
+
+
+
+
+sol = Solution()
+
+nums1 = [1]
+nums2 = [2,5,6]
+
+
+print(sol.findMedianSortedArrays(nums1, nums2))
+
+
+
+
+
+
--- Q079-recursion_test.py
+++ Q079-recursion_test.py
@@ -5,6 +5,25 @@ When I'm debugging Q079, it seems calling a class method
 recursively will alter the value of the interior variable.
 I'm testing the problem here and see what I can do to change
 it.
+
+The result of test:
+yes, unlike C, python doesn't create a new closed space
+every time a class method is called.
+I would guess if the method is @staticmethod, it would behave
+similar to C.
+
+This must have something to do with the famous saying that
+"we are all consenting adults."
+And python allows very open access to all the variables within
+a class.
+
+The solution I came up with:
+I will just use the same variable to work at the recursion.
+I just need to adjust the value of the variable every time
+a recursion takes place or a recursion is finished.
+
+I could also make the method static. Didn't go for it 'cause
+it's gonna be a lot of arguments passed to the funciton.
 """
 
 class RecursionTest():
