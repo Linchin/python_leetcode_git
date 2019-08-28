84e7fc535b0f9440e3bc0b755e5858a38a662f52
Linchin <lingqing.gan@gmail.com>
2019-08-28 11:13:18 -0400

    Q005 lab version

---
+++ Q005_method01_DP.py
@@ -0,0 +1,24 @@
+"""
+Q005 Longest Palindromic Substring
+Medium
+
+08/27/2019
+String. Dynamic programming.
+Methods:
+    Dynamic programming;
+    Manacher's algo;
+    Palidromic tree;
+
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+Input: "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: "cbbd"
+Output: "bb"
+"""
