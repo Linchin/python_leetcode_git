ddb1ee312c0ea2f393f1296f7d1c81564b0a5680
Linchin <lingqing.gan@gmail.com>
2019-09-03 15:05:46 -0400

    Q 771 Jewels and Stones (Easy)

---
+++ Q771.py
@@ -0,0 +1,74 @@
+"""
+Q771
+
+Jewels and Stones
+Easy
+
+\ hash table\
+
+...
+method:
+collections
+```
+
+You're given strings J representing the types of stones that are jewels,
+and S representing the stones you have.  Each character in S is a type
+of stone you have.  You want to know how many of the stones you have
+are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and
+S are letters. Letters are case sensitive, so "a" is considered a
+different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+"""
+from collections import defaultdict
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+
+        jewels = defaultdict()
+
+        c = 0
+
+        for item in J:
+
+            jewels[item] = 0
+
+        for item in S:
+
+            if item in jewels:
+
+                c += 1
+
+        return c
+
+
+
+sol = Solution()
+
+J = "aA"
+S = "bbbb"
+
+print(sol.numJewelsInStones(J, S))
+
+
+
+
+
+
+
+
+
+
