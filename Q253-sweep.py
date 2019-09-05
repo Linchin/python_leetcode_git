8597ec8c47de1fb3eab0576aaba340e94b0fd99e
Linchin <lingqing.gan@gmail.com>
2019-09-05 11:15:18 -0400

    Q 253 done Meeting room II Medium

--- Q253-sweep.py
+++ Q253-sweep.py
@@ -21,12 +21,41 @@ Input: [[7,10],[2,4]]
 Output: 1
 """
 
+from typing import List
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 
+        if not intervals:
+            return 0
+
+        total_room = 1
+
+        end_time = [-1]
+
+        sorted_start = sorted(intervals, key=lambda item: item[0])
+
+        for item in sorted_start:
+
+            if item[0] < min(end_time):
+                total_room += 1
+                end_time.append(item[1])
+
+            else:
+                index = end_time.index(min(end_time))
+                end_time[index] = item[1]
+
+        return total_room
 
 
+
+
+a = [[0, 30],[5, 10],[15, 20]]
+b = [[7,10],[2,4]]
+c = [[7,10]]
+d = []
+sol = Solution()
+print(sol.minMeetingRooms(d))
 
 
 
