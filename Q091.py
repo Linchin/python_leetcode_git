40e7e31bc474c9b56c5b6d2d5b26d56224e59565
Linchin <lingqing.gan@gmail.com>
2019-09-18 17:01:51 -0400

    Q091 done decode ways medium
    Corner cases are the tricky part.

--- Q091.py
+++ Q091.py
@@ -30,9 +30,30 @@ Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF"
 
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
+        if s == "" or s[0] == "0":
+            return 0
 
+        t1 = 1
+        t2 = 1
+        t3 = 1
 
+        for i in range(1, len(s)):
+
+            t3 = 0
+
+            # single
+            if s[i] != "0":
+                t3 += t2
+            # double
+            if "10" <= s[i-1:i+1] < "27":
+                t3 += t1
+            t1, t2 = t2, t3
+
+        return t3
+
+sol = Solution()
+a = "1001"
+print(sol.numDecodings(a))
 
 
 
