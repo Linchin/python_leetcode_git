d76dae1103c74b4ee37872f12bfd7e1286a12850
Linchin <lingqing.gan@gmail.com>
2019-12-23 16:58:39 -0500

    update

--- Q146.py
+++ Q146.py
@@ -21,7 +21,8 @@ The cache is initialized with a positive capacity.
 Follow up:
 Could you do both operations in O(1) time complexity?
 
-
+notes:
+linkded list + dict(hash map)
 
 """
 
@@ -29,9 +30,47 @@ Could you do both operations in O(1) time complexity?
 
 class LRUCache:
 
-    def __init__(self, capacity: int):
+    class Node:
+        def __init__(self, key, val):
+            self.key = key
+            self.val = val
+            self.next = None
+            self.prev = None
 
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.size = 0
+        self.hash = {}
+        self.head = None
+        self.tail = None
+
+    def add(self, key: int, value: int):
+        # add a node to the end
+        self.hash[key] = self.Node(key, value)
+        self.tail = self.hash[key]
+        self.size += 1
+
+    def remove(self, key: int):
+        # remove a node
+        # if the node is 1st
+        if self.hash[key].prev is None:
+            self.size -= 1
+            self.head = self.hash[key].next
+        # if the node is last
+        elif self.hash[key].next is None:
+            self.size -= 1
+            self.head = self.hash[key].prev
+        # if the node is in the middle
+        else:
+            self.size -= 1
+            self.hash[key].prev.next = self.hash[key].next
+            self.hash[key].next.prev = self.hash[key].prev
+        # remove the node from hash table
+        del self.hash[key]
+        
     def get(self, key: int) -> int:
+        if key not in self.hash:
+            return -1
 
     def put(self, key: int, value: int) -> None:
 
--- Q292.py
+++ Q292.py
@@ -7,7 +7,7 @@ Easy
 
 You are playing the following Nim Game with your friend:
 There is a heap of stones on the table, each time one of
-you take turns to remove 1 to 3 stones. The one who removes
+you take turns to remove 1 TO 3 stones. The one who removes
 the last stone will be the winner. You will take the first
 turn to remove the stones.
 
@@ -15,12 +15,11 @@ Both of you are very clever and have optimal strategies
 for the game. Write a function to determine whether you
 can win the game given the number of stones in the heap.
 
+notes:
+math induction
 """
 
 
 class Solution:
     def canWinNim(self, n: int) -> bool:
-
-
-
-
+        return n % 4 != 0
